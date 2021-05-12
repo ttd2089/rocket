9bdeefc3305d96e6bd8e99488281cb0ec5a738b6
Thomas Showers <tshowers@beyondtrust.com>
2021-05-12 11:38:38 -0300

    init

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+extern crate notify;
+
+use notify::{Watcher, RecursiveMode, watcher};
+use std::sync::mpsc::channel;
+use std::time::Duration;
+
+fn main() {
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+
+    // Create a watcher object, delivering debounced events.
+    // The notification back-end is selected based on the platform.
+    let mut watcher = watcher(tx, Duration::from_secs(3)).unwrap();
+
+    // Add a path to be watched. All files and directories at that path and
+    // below will be monitored for changes.
+    watcher.watch(".", RecursiveMode::Recursive).unwrap();
+
+    loop {
+        match rx.recv() {
+           Ok(event) => println!("{:?}", event),
+           Err(e) => println!("watch error: {:?}", e),
+        }
+    }
+}
