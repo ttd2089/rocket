d860ea077204c94fa2487d015dd07effc76b7389
Thomas Showers <tshowers@beyondtrust.com>
2021-05-12 13:23:03 -0300

    Respect --change-directory

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,49 @@ extern crate notify;
 #[macro_use]
 extern crate clap;
 
-use notify::{Watcher, RecursiveMode, watcher};
+use clap::{App, Arg};
+use notify::{watcher, RecursiveMode, Watcher};
 use std::sync::mpsc::channel;
 use std::time::Duration;
-use clap::{Arg,App};
 
 fn main() {
-
     let matches = App::new("Rocket - Pocket rewritten in rust")
         .version(crate_version!())
         .author(crate_authors!())
         .about(crate_description!())
-        .arg(Arg::with_name("change-directory")
-            .short("-C")
-            .long("--change-directory")
-            .value_name("Change directory")
-            .help("Sets the working directory of the associated command"))
+        .arg(
+            Arg::with_name("change-directory")
+                .short("-C")
+                .long("--change-directory")
+                .value_name("directory")
+                .help("Sets the working directory of the associated command"),
+        )
         .get_matches();
 
-    let dir = matches.value_of("change-directory").unwrap_or("Fire fire fire");
-    println!("{}",dir);
-    // Create a channel to receive the events.
+    let dir = matches
+        .value_of("change-directory")
+        .map(|s| s.to_string())
+        .or_else(|| {
+            std::env::current_dir()
+                .ok()
+                .and_then(|p| p.into_os_string().into_string().ok())
+        })
+        .unwrap_or(".".to_string());
+
+    watch_directory(&dir);
+}
+
+fn watch_directory(dir: &str) {
     let (tx, rx) = channel();
 
-    // Create a watcher object, delivering debounced events.
-    // The notification back-end is selected based on the platform.
-    let mut watcher = watcher(tx, Duration::from_secs(3)).unwrap();
+    let mut watcher = watcher(tx, Duration::from_secs(1)).unwrap();
 
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
-    watcher.watch(".", RecursiveMode::Recursive).unwrap();
+    watcher.watch(dir, RecursiveMode::Recursive).unwrap();
 
     loop {
         match rx.recv() {
-           Ok(event) => println!("{:?}", event),
-           Err(e) => println!("watch error: {:?}", e),
+            Ok(event) => println!("{:?}", event),
+            Err(e) => println!("watch error: {:?}", e),
         }
     }
 }
